87d21607aee3afaccfdb22db2f90564441ba2b14
yuguoyu123456 <52940163+yuguoyu123456@users.noreply.github.com>
2026-09-03 13:47:07 +0800

    feat: add offline shell cache for web viewer

---
+++ integrations/mobile/web_pwa/service-worker.js
@@ -0,0 +1,21 @@
+const CACHE = 'ads1299-core-web-viewer-v1';
+const ASSETS = ['./', './index.html', './app.js', './manifest.webmanifest'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(hit => hit || fetch(event.request))
+  );
+});
