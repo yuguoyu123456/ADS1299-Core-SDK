fb02f57d6937932c2c54bbc84b78d96950df7409
yuguoyu123456 <52940163+yuguoyu123456@users.noreply.github.com>
2026-09-08 11:44:24 +0800

    feat(puya): add shared ADS1299 ready app header

---
+++ firmware/mcu/19_Puya/_shared/puya_ads1299_ready_app.h
@@ -0,0 +1,39 @@
+#ifndef PUYA_ADS1299_READY_APP_H
+#define PUYA_ADS1299_READY_APP_H
+
+#include <stdint.h>
+#include "ads1299.h"
+#include "ads1299_profiles.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef enum {
+    PUYA_ADS1299_PROFILE_EEG_250 = 0,
+    PUYA_ADS1299_PROFILE_EEG_500,
+    PUYA_ADS1299_PROFILE_EEG_1000,
+    PUYA_ADS1299_PROFILE_INTERNAL_TEST,
+    PUYA_ADS1299_PROFILE_INPUT_SHORT
+} puya_ads1299_profile_t;
+
+typedef struct {
+    uint32_t sample_index;
+    uint32_t read_errors;
+    uint32_t queue_overflows;
+    ads1299_frame_t last_frame;
+} puya_ads1299_state_t;
+
+ads1299_status_t puya_ads1299_prepare(ads1299_t *dev,
+                                      puya_ads1299_profile_t profile,
+                                      int verify_registers);
+ads1299_status_t puya_ads1299_start(ads1299_t *dev);
+ads1299_status_t puya_ads1299_read_one(ads1299_t *dev,
+                                       puya_ads1299_state_t *state);
+ads1299_status_t puya_ads1299_stop(ads1299_t *dev);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
