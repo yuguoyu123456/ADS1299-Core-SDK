e496ae9e1f0f465c8919201a3712097a1c5860d1
Guoyu Yu <52940163+yuguoyu123456@users.noreply.github.com>
2026-09-09 05:01:58 +0800

    firmware: add STM32F091 beginner demo API

---
+++ firmware/mcu/01_STMicroelectronics/STM32F091/examples/stm32f091_beginner_demo.h
@@ -0,0 +1,16 @@
+#ifndef STM32F091_ADS1299_BEGINNER_DEMO_H
+#define STM32F091_ADS1299_BEGINNER_DEMO_H
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int stm32f091_ads1299_beginner_demo(uint32_t stream_frames);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
