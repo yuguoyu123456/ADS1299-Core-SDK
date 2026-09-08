b62469e35117b701a2fe6b9c6a19792f8329d714
Guoyu Yu <52940163+yuguoyu123456@users.noreply.github.com>
2026-09-09 05:01:36 +0800

    firmware: add STM32F091 example platform header

---
+++ firmware/mcu/01_STMicroelectronics/STM32F091/examples/stm32f091_example_platform.h
@@ -0,0 +1,22 @@
+#ifndef STM32F091_ADS1299_EXAMPLE_PLATFORM_H
+#define STM32F091_ADS1299_EXAMPLE_PLATFORM_H
+
+#include <stddef.h>
+#include <stdint.h>
+#include "ads1299_spi.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int stm32f091_ads1299_make_hal(ads1299_platform_hal_t *hal);
+int board_ads1299_hal(ads1299_platform_hal_t *hal);
+void stm32f091_example_log(const char *text);
+int stm32f091_example_write(const uint8_t *data, size_t len);
+uint32_t stm32f091_example_timestamp_us(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
