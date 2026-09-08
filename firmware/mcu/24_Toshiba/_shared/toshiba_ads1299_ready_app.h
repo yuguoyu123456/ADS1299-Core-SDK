374550884f16ae8fa1c94ed861b24ff0bc38a239
yuguoyu123456 <52940163+yuguoyu123456@users.noreply.github.com>
2026-09-08 11:51:29 +0800

    feat(toshiba): add shared ADS1299 ready app header

---
+++ firmware/mcu/24_Toshiba/_shared/toshiba_ads1299_ready_app.h
@@ -0,0 +1,39 @@
+#ifndef TOSHIBA_ADS1299_READY_APP_H
+#define TOSHIBA_ADS1299_READY_APP_H
+
+#include <stdint.h>
+#include "ads1299.h"
+#include "ads1299_profiles.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef enum {
+    TOSHIBA_ADS1299_PROFILE_EEG_250 = 0,
+    TOSHIBA_ADS1299_PROFILE_EEG_500,
+    TOSHIBA_ADS1299_PROFILE_EEG_1000,
+    TOSHIBA_ADS1299_PROFILE_INTERNAL_TEST,
+    TOSHIBA_ADS1299_PROFILE_INPUT_SHORT
+} toshiba_ads1299_profile_t;
+
+typedef struct {
+    uint32_t sample_index;
+    uint32_t read_errors;
+    uint32_t queue_overflows;
+    ads1299_frame_t last_frame;
+} toshiba_ads1299_state_t;
+
+ads1299_status_t toshiba_ads1299_prepare(ads1299_t *dev,
+                                         toshiba_ads1299_profile_t profile,
+                                         int verify_registers);
+ads1299_status_t toshiba_ads1299_start(ads1299_t *dev);
+ads1299_status_t toshiba_ads1299_read_one(ads1299_t *dev,
+                                          toshiba_ads1299_state_t *state);
+ads1299_status_t toshiba_ads1299_stop(ads1299_t *dev);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
