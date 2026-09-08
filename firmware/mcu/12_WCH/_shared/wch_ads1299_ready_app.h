05c997eb767e3ede5c39609d207975ac66fbdc32
yuguoyu123456 <52940163+yuguoyu123456@users.noreply.github.com>
2026-09-08 11:15:26 +0800

    feat(wch): add shared ADS1299 ready app header

---
+++ firmware/mcu/12_WCH/_shared/wch_ads1299_ready_app.h
@@ -0,0 +1,17 @@
+#ifndef WCH_ADS1299_READY_APP_H
+#define WCH_ADS1299_READY_APP_H
+#include <stdint.h>
+#include "ads1299.h"
+#ifdef __cplusplus
+extern "C" {
+#endif
+typedef enum { WCH_ADS1299_PROFILE_EEG_250=0, WCH_ADS1299_PROFILE_EEG_500, WCH_ADS1299_PROFILE_EEG_1000, WCH_ADS1299_PROFILE_INTERNAL_TEST, WCH_ADS1299_PROFILE_INPUT_SHORT } wch_ads1299_profile_t;
+typedef struct { uint32_t sample_index; uint32_t read_errors; uint32_t queue_overflows; ads1299_frame_t last_frame; } wch_ads1299_ready_state_t;
+ads1299_status_t wch_ads1299_prepare(ads1299_t *dev, wch_ads1299_profile_t profile, int verify_registers);
+ads1299_status_t wch_ads1299_start(ads1299_t *dev);
+ads1299_status_t wch_ads1299_read_one(ads1299_t *dev, wch_ads1299_ready_state_t *state);
+ads1299_status_t wch_ads1299_stop(ads1299_t *dev);
+#ifdef __cplusplus
+}
+#endif
+#endif
