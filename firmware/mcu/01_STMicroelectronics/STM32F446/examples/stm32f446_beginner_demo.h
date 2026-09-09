6341ad5f76e9ad79504a27acb7acb1632ae0118d
Guoyu Yu <52940163+yuguoyu123456@users.noreply.github.com>
2026-09-10 06:02:32 +0800

    firmware: add STM32F446 beginner demo API

---
+++ firmware/mcu/01_STMicroelectronics/STM32F446/examples/stm32f446_beginner_demo.h
@@ -0,0 +1,8 @@
+#ifndef STM32F446_ADS1299_BEGINNER_DEMO_H
+#define STM32F446_ADS1299_BEGINNER_DEMO_H
+
+#include <stdint.h>
+
+int stm32f446_ads1299_beginner_demo(uint32_t stream_frames);
+
+#endif /* STM32F446_ADS1299_BEGINNER_DEMO_H */
