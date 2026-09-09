101845cf8a036f40cd547d0e0b549928e745d2c6
Guoyu Yu <52940163+yuguoyu123456@users.noreply.github.com>
2026-09-09 20:00:31 +0800

    firmware: add STM32F407 beginner demo header

---
+++ firmware/mcu/01_STMicroelectronics/STM32F407/examples/stm32f407_beginner_demo.h
@@ -0,0 +1,16 @@
+#ifndef STM32F407_ADS1299_BEGINNER_DEMO_H
+#define STM32F407_ADS1299_BEGINNER_DEMO_H
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int stm32f407_ads1299_beginner_demo(uint32_t stream_frames);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
