a0ee1c5b085ce40fde12dd3a05131202653b7b13
Guoyu Yu <52940163+yuguoyu123456@users.noreply.github.com>
2026-09-09 09:58:14 +0800

    Add STM32F103 ADS1299 example platform header

---
+++ firmware/mcu/01_STMicroelectronics/STM32F103/examples/stm32f103_example_platform.h
@@ -0,0 +1,22 @@
+#ifndef STM32F103_ADS1299_EXAMPLE_PLATFORM_H
+#define STM32F103_ADS1299_EXAMPLE_PLATFORM_H
+
+#include <stddef.h>
+#include <stdint.h>
+#include "ads1299_spi.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int stm32f103_ads1299_make_hal(ads1299_platform_hal_t *hal);
+int board_ads1299_hal(ads1299_platform_hal_t *hal);
+void stm32f103_example_log(const char *text);
+int stm32f103_example_write(const uint8_t *data, size_t len);
+uint32_t stm32f103_example_timestamp_us(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
