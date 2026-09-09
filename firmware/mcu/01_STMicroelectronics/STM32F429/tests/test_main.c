be620c3364c4ecbfdf59d3ed31b519defa2d87de
Guoyu Yu <52940163+yuguoyu123456@users.noreply.github.com>
2026-09-10 02:03:12 +0800

    test(stm32f429): add unified host test runner

---
+++ firmware/mcu/01_STMicroelectronics/STM32F429/tests/test_main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void test_core_frame_numeric_boundary(void);
+void test_register_boundary_is_opaque(void);
+void test_port_spi(void);
+void test_port_reset_line(void);
+void test_port_drdy_active_low(void);
+
+int main(void) {
+    test_core_frame_numeric_boundary();
+    test_register_boundary_is_opaque();
+    test_port_spi();
+    test_port_reset_line();
+    test_port_drdy_active_low();
+    puts("PASS STM32F429 integration smoke tests");
+    return 0;
+}
