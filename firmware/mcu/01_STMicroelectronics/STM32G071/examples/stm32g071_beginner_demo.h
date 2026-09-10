05555dac24a90a237ab36b7624c73ea8bd8aeef7
Guoyu Yu <52940163+yuguoyu123456@users.noreply.github.com>
2026-09-10 16:04:25 +0800

    firmware: add STM32G071 ADS1299 beginner demo interface

---
+++ firmware/mcu/01_STMicroelectronics/STM32G071/examples/stm32g071_beginner_demo.h
@@ -0,0 +1,8 @@
+#ifndef STM32G071_ADS1299_BEGINNER_DEMO_H
+#define STM32G071_ADS1299_BEGINNER_DEMO_H
+
+#include <stdint.h>
+
+int stm32g071_ads1299_beginner_demo(uint32_t stream_frames);
+
+#endif /* STM32G071_ADS1299_BEGINNER_DEMO_H */
