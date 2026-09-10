2fc36e15e28ae01fe05eef59a93589d1e61d3f3e
Guoyu Yu <52940163+yuguoyu123456@users.noreply.github.com>
2026-09-10 11:01:06 +0800

    STM32F767: add beginner demo interface

---
+++ firmware/mcu/01_STMicroelectronics/STM32F767/examples/stm32f767_beginner_demo.h
@@ -0,0 +1,8 @@
+#ifndef STM32F767_ADS1299_BEGINNER_DEMO_H
+#define STM32F767_ADS1299_BEGINNER_DEMO_H
+
+#include <stdint.h>
+
+int stm32f767_ads1299_beginner_demo(uint32_t stream_frames);
+
+#endif /* STM32F767_ADS1299_BEGINNER_DEMO_H */
