3ee6d04396549b4c30e58fe17a68a12ce3cc301d
Guoyu Yu <52940163+yuguoyu123456@users.noreply.github.com>
2026-09-11 03:01:54 +0800

    test(stm32h533): add unified ADS1299 integration test runner

---
+++ firmware/mcu/01_STMicroelectronics/STM32H533/tests/test_main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void test_core_frame_numeric_boundary(void);
+void test_register_boundary_is_opaque(void);
+void test_port_spi(void);
+void test_port_reset_line(void);
+void test_port_drdy_active_low(void);
+void test_canonical_packet_round_trip(void);
+
+int main(void) {
+    test_core_frame_numeric_boundary();
+    test_register_boundary_is_opaque();
+    test_port_spi();
+    test_port_reset_line();
+    test_port_drdy_active_low();
+    test_canonical_packet_round_trip();
+    puts("PASS STM32H533 integration smoke tests");
+    return 0;
+}
