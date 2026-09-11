ed116aa354d8c9559adc81a7246e02ec79bb4f2b
Guoyu Yu <52940163+yuguoyu123456@users.noreply.github.com>
2026-09-11 14:59:47 +0800

    test(stm32f407): run canonical packet regression

--- firmware/mcu/01_STMicroelectronics/STM32F407/tests/test_main.c
+++ firmware/mcu/01_STMicroelectronics/STM32F407/tests/test_main.c
@@ -5,6 +5,7 @@ void test_register_boundary_is_opaque(void);
 void test_port_reset_line(void);
 void test_port_spi(void);
 void test_port_drdy_active_low(void);
+void test_canonical_packet_round_trip(void);
 
 static int run_test(const char *name, void (*fn)(void)) {
     printf("[ RUN      ] %s\n", name);
@@ -21,6 +22,7 @@ int main(void) {
     failures += run_test("port_spi", test_port_spi);
     failures += run_test("port_reset_line", test_port_reset_line);
     failures += run_test("port_drdy_active_low", test_port_drdy_active_low);
+    failures += run_test("canonical_packet_round_trip", test_canonical_packet_round_trip);
 
     if (failures != 0) {
         printf("FAIL STM32F407 integration smoke tests (%d failures)\n", failures);
