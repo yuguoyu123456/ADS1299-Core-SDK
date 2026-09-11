701917bec4a2852ae85f1f14c5662d07eaaba0af
Guoyu Yu <52940163+yuguoyu123456@users.noreply.github.com>
2026-09-11 20:05:04 +0800

    test(stm32h533): execute frame queue regressions

--- firmware/mcu/01_STMicroelectronics/STM32H533/tests/test_main.c
+++ firmware/mcu/01_STMicroelectronics/STM32H533/tests/test_main.c
@@ -6,6 +6,9 @@ void test_port_spi(void);
 void test_port_reset_line(void);
 void test_port_drdy_active_low(void);
 void test_canonical_packet_round_trip(void);
+void test_frame_queue_fifo_wraparound_and_overflow(void);
+void test_frame_queue_argument_validation(void);
+void test_frame_queue_long_running_interleaving(void);
 
 int main(void) {
     test_core_frame_numeric_boundary();
@@ -14,6 +17,9 @@ int main(void) {
     test_port_reset_line();
     test_port_drdy_active_low();
     test_canonical_packet_round_trip();
+    test_frame_queue_fifo_wraparound_and_overflow();
+    test_frame_queue_argument_validation();
+    test_frame_queue_long_running_interleaving();
     puts("PASS STM32H533 integration smoke tests");
     return 0;
 }
