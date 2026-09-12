3c4c5ca82446fcdc6c89b91c35034316b5b1f2c6
Guoyu Yu <52940163+yuguoyu123456@users.noreply.github.com>
2026-09-13 02:03:41 +0800

    feat(esp32-p4): add ESP-IDF HAL binding interface

---
+++ firmware/mcu/02_Espressif/ESP32-P4/ads1299_port/ads1299_espidf_hal.h
@@ -0,0 +1,14 @@
+#ifndef ADS1299_ESP32P4_ESPIDF_HAL_H
+#define ADS1299_ESP32P4_ESPIDF_HAL_H
+
+#include "ads1299_spi.h"
+#include "driver/spi_master.h"
+
+typedef struct {
+    spi_device_handle_t spi;
+} ads1299_espidf_hal_ctx_t;
+
+int ads1299_espidf_hal_init(ads1299_espidf_hal_ctx_t *ctx,
+                            ads1299_platform_hal_t *hal);
+
+#endif
