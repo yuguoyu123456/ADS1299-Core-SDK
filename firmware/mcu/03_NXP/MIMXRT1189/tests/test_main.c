fc01e7e4064a5cda97bb0b704be3d1ba1cc0766f
Guoyu Yu <52940163+yuguoyu123456@users.noreply.github.com>
2026-09-21 17:01:43 +0800

    test(rt1189): run delay routing in platform smoke suite

--- firmware/mcu/03_NXP/MIMXRT1189/tests/test_main.c
+++ firmware/mcu/03_NXP/MIMXRT1189/tests/test_main.c
@@ -5,6 +5,7 @@ void test_port_reset_line(void);
 void test_core_frame_numeric_boundary(void);
 void test_register_constants(void);
 void test_control_gpio_routing(void);
+void test_delay_routing(void);
 
 int main(void)
 {
@@ -13,6 +14,7 @@ int main(void)
     test_core_frame_numeric_boundary();
     test_register_constants();
     test_control_gpio_routing();
+    test_delay_routing();
 
     puts("MIMXRT1189 ADS1299 platform smoke tests: PASS");
     return 0;
