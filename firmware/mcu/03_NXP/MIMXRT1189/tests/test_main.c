694e589de0e0976702c11682bd0f92f5f133a177
Guoyu Yu <52940163+yuguoyu123456@users.noreply.github.com>
2026-09-21 16:02:33 +0800

    test(rt1189): run control GPIO routing smoke test

--- firmware/mcu/03_NXP/MIMXRT1189/tests/test_main.c
+++ firmware/mcu/03_NXP/MIMXRT1189/tests/test_main.c
@@ -4,6 +4,7 @@ void test_port_spi(void);
 void test_port_reset_line(void);
 void test_core_frame_numeric_boundary(void);
 void test_register_constants(void);
+void test_control_gpio_routing(void);
 
 int main(void)
 {
@@ -11,6 +12,7 @@ int main(void)
     test_port_reset_line();
     test_core_frame_numeric_boundary();
     test_register_constants();
+    test_control_gpio_routing();
 
     puts("MIMXRT1189 ADS1299 platform smoke tests: PASS");
     return 0;
