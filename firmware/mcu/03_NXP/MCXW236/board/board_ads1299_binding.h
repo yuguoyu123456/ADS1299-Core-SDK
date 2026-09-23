3ff90d34fb980a44a67201bee5f825bcd3e511ec
Guoyu Yu <52940163+yuguoyu123456@users.noreply.github.com>
2026-09-23 08:02:49 +0800

    MCXW236: add board-to-HAL binding API

---
+++ firmware/mcu/03_NXP/MCXW236/board/board_ads1299_binding.h
@@ -0,0 +1,19 @@
+#ifndef BOARD_ADS1299_BINDING_H
+#define BOARD_ADS1299_BINDING_H
+
+#include "../ads1299_port/ads1299_spi.h"
+
+/*
+ * Build the MCXW236 ADS1299 platform HAL from board_ads1299_config.h.
+ *
+ * Returns:
+ *   0  configuration accepted and HAL returned
+ *  -1  out_hal is NULL
+ *  -2  board configuration is incomplete or has not been reviewed
+ *  -3  MCXW236 vendor HAL initialization failed
+ *
+ * The context backing the returned callbacks has static storage duration.
+ */
+int board_ads1299_make_hal(ads1299_platform_hal_t *out_hal);
+
+#endif /* BOARD_ADS1299_BINDING_H */
